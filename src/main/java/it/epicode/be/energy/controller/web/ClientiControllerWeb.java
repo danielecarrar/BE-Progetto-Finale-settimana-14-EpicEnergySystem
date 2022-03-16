826f20d8ad9c1b7fb63c8c8774bcab6d1d829c60
danielecarrar <daniele._k@libero.it>
2022-03-16 17:32:29 +0100

    added code to add customers

--- src/main/java/it/epicode/be/energy/controller/web/ClientiControllerWeb.java
+++ src/main/java/it/epicode/be/energy/controller/web/ClientiControllerWeb.java
@@ -1,5 +1,7 @@
 package it.epicode.be.energy.controller.web;
 
+import java.util.Optional;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -26,7 +29,7 @@ public class ClientiControllerWeb {
 	@GetMapping("/mostraelenco")
 	public ModelAndView mostraElencoClienti() {
 		log.info("Visualizzazione elenco clienti presenti su pagina HTML");
-		ModelAndView view = new ModelAndView("elencoclienti");// nome HTML
+		ModelAndView view = new ModelAndView("elencoclienti"); // nome HTML
 		view.addObject("listaClienti", clienteService.findAll());
 
 		return view;
@@ -34,18 +37,49 @@ public class ClientiControllerWeb {
 
 	@GetMapping("/mostraformaggiungi")
 	public String mostraFormAggiungi(Cliente cliente, Model model) {
-		log.info("form aggiunta di un cliente");
 		return "formcliente";
 	}
 
-	@PostMapping("/addcliente")
-	public String aggiungiCliente(Cliente cliente, BindingResult result, Model model) {
-		log.info("aggiunta cliente");
+	@PostMapping("/aggiungicliente")
+	public String aggiungiCliente(@Valid Cliente cliente, BindingResult result, Model model) {
+		cliente.setDataInserimento();
+
 		if (result.hasErrors()) {
-			
 			return "formcliente";
 		}
 		clienteService.save(cliente);
 		return "redirect:/clienti/mostraelenco";
+	}
+
+	@GetMapping("/mostraformaggiorna/{id}")
+	public ModelAndView mostraFormAggiorna(@PathVariable Long id, Model model) {
+
+		Optional<Cliente> clienteTmp = clienteService.findById(id);
+		if (clienteTmp.isPresent()) {
+			ModelAndView view = new ModelAndView("aggiornacliente"); // HTML
+			view.addObject("cliente", clienteTmp.get());
+			return view;
+		}
+		return new ModelAndView("error").addObject("message", "Cliente con id " + id + " non trovato!");
+	}
+
+	@PostMapping("/aggiornacliente/{id}")
+	public String aggiornaCliente(@PathVariable Long id, Cliente cliente, BindingResult result, Model model) {
+		clienteService.update(id, cliente);
+		return "redirect:/clienti/mostraelenco";
+	}
+
+	@GetMapping("/eliminacliente/{id}")
+	public ModelAndView eliminaCliente(@PathVariable Long id, Model model) {
+		Optional<Cliente> clienteDaEliminare = clienteService.findById(id);
+		if (clienteDaEliminare.isPresent()) {
+			clienteService.delete(id);
+			ModelAndView view = new ModelAndView("elencoclienti");
+			view.addObject("listaClienti", clienteService.findAll());
+			return view;
+		} else {
+			return new ModelAndView("error").addObject("messaggio",
+					"Nessun cliente con id " + id + " presente nel database!");
+		}
 	}
 }
